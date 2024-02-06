c557e498355ec4610f711f8430b571efc51710ac
rajeshCreate72 <mechdepartment1221@gmail.com>
2024-02-07 00:12:14 +0530

    TextBoxAdded

--- frontEnd-react/src/Components/ChatResponseComponent/ChatResponse.js
+++ frontEnd-react/src/Components/ChatResponseComponent/ChatResponse.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import './ChatsResponseStyle.css';
 import ChatResponseTopBar from "./ChatResponseTopBar";
+import ResponseTextBox from "./ResponseTextBox";
 
 class ChatResponse extends Component {
     render() {
         return (
             <div className="chat-response">
                 <ChatResponseTopBar />
+                <ResponseTextBox />
             </div>
         )
     }
---
+++ frontEnd-react/src/Components/ChatResponseComponent/ResponseTextBox.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './ChatsResponseStyle.css'
+
+class ResponseTextBox  extends Component {
+  render() {
+    return (
+      <div className="response-text-box">
+        <textarea className="response-text-input" type="text" ></textarea>
+      </div>
+    )
+  }
+}
+
+export default ResponseTextBox; 
