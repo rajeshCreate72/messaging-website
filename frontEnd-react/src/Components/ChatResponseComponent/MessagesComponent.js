3e5aa42ac12a6983d68946b4824a52a76b2e7db5
rajeshCreate72 <mechdepartment1221@gmail.com>
2024-02-09 23:25:03 +0530

    Messages Added Inorder of the msg array

--- frontEnd-react/src/Components/ChatResponseComponent/MessagesComponent.js
+++ frontEnd-react/src/Components/ChatResponseComponent/MessagesComponent.js
@@ -1,10 +1,31 @@
 import React from 'react'
 
 function MessagesComponent() {
-  
+  const user1 = "1234"
+  const user2 = "1236"
+  const msg = [
+    {user: user1, msg: "Hi", timeStamp: "45:60"},
+    {user: user2, msg: "Hello", timeStamp: "85:15"},
+    {user: user1, msg: "How Are you?", timeStamp: "64:52"},
+    {user: user2, msg: "i am good", timeStamp: "82:53"}
+  ]
   return (
     <div className='msg-container'>
-
+      {msg.map((message, index) => (
+        message.user === user1 ? (
+          <div className="my-msg" key={index}>
+            <p>{ message.msg }</p>
+            <br />
+            <p>{ message.timeStamp }</p>
+          </div>
+        ) : (
+            <div className='not-my-msg' key={index}>
+              <p>{ message.msg }</p>
+              <br />
+              <p>{ message.timeStamp }</p>
+            </div>
+        )
+      ))}
     </div>
   )
 }
