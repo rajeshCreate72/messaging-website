08d60afbf3dff78315c4efcde82a18178a9e61ec
rajeshCreate72 <mechdepartment1221@gmail.com>
2024-02-12 22:48:04 +0530

    Contacts list added Style not correct

--- frontEnd-react/src/Components/ChatsComponent/ChatsContacts.js
+++ frontEnd-react/src/Components/ChatsComponent/ChatsContacts.js
@@ -2,8 +2,25 @@ import React from 'react'
 import "./ChatsStyle.css"
 
 function ChatsContacts() {
+  const userId1 = "452145"
+  const userId2 = "452174"
+  const contactsList = [
+    {user: userId1, userName: "HelloGuru"},
+    {user: userId2, userName: "HelloFriEnd"}
+  ]
+
   return (
-    <div className='chats-contacts'></div>
+    <div className='chats-contacts'>
+      {contactsList.map((contacts, index) => (
+        <div className='contacts-list' key={ index } style={{display: 'inline-block'}}>
+          <form>
+            <li style={{listStyle: 'none'}}>
+              <button>{contacts.userName}</button>
+            </li>
+          </form>
+        </div>
+      )) }
+    </div>
   )
 }
 
