23447e6078b780d5b5d8cd0570d201a337b6036b
rajeshCreate72 <mechdepartment1221@gmail.com>
2024-02-19 15:42:34 +0530

    Contacts Padding changed

--- frontEnd-react/src/Components/ChatResponseComponent/MessageTextBox.js
+++ frontEnd-react/src/Components/ChatResponseComponent/MessageTextBox.js
@@ -2,6 +2,7 @@ import React from 'react'
 import sendLogo from "../contents/sendLogo.png"
 
 function MessageTextBox() {
+  // const [textBoxValue, useTextBoxValue] = useState("")
   return (
     <div className='text-box-container'>
       <form className='message-box-form'>
