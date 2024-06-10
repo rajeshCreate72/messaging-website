6e72c35269ac062172faa56a21e4aa499220ad6c
rajeshCreate72 <mechdepartment1221@gmail.com>
2024-06-11 02:37:14 +0530

    Search contacts added

--- client/src/Components/ChatLeftEle/ChatLeftInfo/ChatLeftInfo.jsx
+++ client/src/Components/ChatLeftEle/ChatLeftInfo/ChatLeftInfo.jsx
@@ -1,39 +1,57 @@
 import React, { useEffect, useRef, useState } from 'react'
+import SearchContact from './SearchContact'
 
 function ChatLeftInfo(props) {
   const [dropDown, setDropdown] = useState(false)
+  const [searchContact, setSearchContact] = useState(false)
   const dropRef = useRef(null)
+  const addBtn = useRef(null)
 
   const handleDropdown = () => {
     setDropdown(!dropDown)
   }
 
-  const handleCloseEvent = (event) => {
-    if(dropRef.current && !dropRef.current.contains(event.target)) {
-      setDropdown(false)
-    }
+  const handleSearchContact = () => {
+    setSearchContact(!searchContact)
   }
 
   useEffect (() => {
-    document.addEventListener('click', handleCloseEvent);
+    const handleCloseEvent = (event) => {
+      if(dropRef.current && !dropRef.current.contains(event.target)) {
+        setDropdown(false)
+      }
+    }
 
-    return () => {document.removeEventListener('click', handleCloseEvent)}
+    document.addEventListener('click', handleCloseEvent)
+
+    return () => {
+      document.removeEventListener('click', handleCloseEvent)
+    }
   }, [])
+
+  
 
   return (
     <div className='info-lt'>
         <form type='GET'>
-          <input type='search' formAction='/search' ></input>
+          <input className='search-box-info' type='search' formAction='/search' placeholder='Enter a contact...'></input>
         </form>
-        <div className='drop-down'>
+        <div className='drop-down' ref={dropRef}>
           <button className='drop-button' onClick={handleDropdown}><h5>&#x22EE;</h5></button>
-          {dropDown && (<div className="drop-content" ref={dropRef}>
+          {dropDown && (<div className="drop-content">
             <ul style={{listStyle: 'none'}}>
-              <li><button onClick={handleDropdown} className='add-btn'><h5>Add Contact</h5></button></li>
+              <li><button onClick={() => {
+                  handleDropdown()
+                  handleSearchContact()
+                }} className='add-btn' ref={addBtn}><h5>Add Contact</h5></button></li>
               <li><button onClick={handleDropdown} className='logout-btn'><h5>Log Out</h5></button></li>
             </ul>
           </div>)}
         </div>
+        {searchContact && (<div className="add-contact-search">
+          <SearchContact />
+          <button onClick={handleSearchContact}>Close</button>
+        </div>)}
     </div>
   )
 }
---
+++ client/src/Components/ChatLeftEle/ChatLeftInfo/SearchContact.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import '../ChatLeftEle.css'
+import axios from 'axios';
+
+function SearchContact() {
+  const [toSearchUser, setToSearchUser] = useState('');
+  const [users, setUsers] = useState([])
+
+  const handleToSearchUser = (event) => {
+    setToSearchUser(event.target.value)
+  }
+
+  const usersData = {};
+
+  const usersList = async(event) => {
+    // event.preventDefault()
+    try {
+      const response = await axios.get('http://localhost:8000/api/register')
+      usersData = response.data
+
+      console.log(usersData)
+    }
+    catch(error) {
+      console.log("Error retriveing users data", error.message)
+    }
+  }
+  usersList()
+
+  return (
+    <div className='search-contact'>
+        <form typeof='GET'>
+            <input 
+              type="search"
+              placeholder="Enter the user's username..."
+              value={toSearchUser}
+              onChange={handleToSearchUser}
+            />
+        </form>
+        <div className='get-contacts'>Getting those contacts...</div>
+    </div>
+  )
+}
+
+export default SearchContact
