fa7a7fee6e1b49b3bd7bd0dcbe1638ab1aebad63
rajeshCreate72 <mechdepartment1221@gmail.com>
2024-06-13 02:16:49 +0530

    Login State handled, Database connection secured

--- server/DatabaseConnection.js
+++ server/DatabaseConnection.js
@@ -1,4 +1,4 @@
-const url = 'mongodb+srv://chatWebsite:@appcluster.g9lvaj1.mongodb.net/chatWebsite?retryWrites=true&w=majority&appName=appCluster'
+const url = 'mongodb+srv://chatWebsite:websiteL1@appcluster.g9lvaj1.mongodb.net/chatWebsiite?retryWrites=true&w=majority&appName=appCluster'
 //mongodb+srv://chatWebsite:<password>@appcluster.g9lvaj1.mongodb.net/?retryWrites=true&w=majority&appName=appCluster
 const mongoose = require('mongoose');
 
