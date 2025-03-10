db77d8d4e1f71e4c8578dee4b991735633a48473
rajeshCreate72 <mechdepartment1221@gmail.com>
2025-03-10 15:36:11 +0530

    contacts changes

--- client/src/Components/ChatLeftEle/ChatLeftContacts/HandleContacts.jsx
+++ client/src/Components/ChatLeftEle/ChatLeftContacts/HandleContacts.jsx
@@ -5,46 +5,46 @@ import { getContacts } from "../../service/actions/syncContacts";
 import { getMessages } from "../../service/actions/syncMessages";
 
 function HandlingContacts() {
-  const { contacts } = useSelector((state) => state.addContacts);
-  const { isLogged } = useSelector((state) => state.loginAuth);
-  const userId = window.localStorage.getItem("userId");
-  const dispatch = useDispatch();
+    const { contacts } = useSelector((state) => state.addContacts);
+    const { isLogged } = useSelector((state) => state.loginAuth);
+    const userId = window.localStorage.getItem("userId");
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    if (isLogged) {
-      dispatch(getContacts(userId));
-    }
-  }, [dispatch, isLogged]);
+    useEffect(() => {
+        if (isLogged) {
+            dispatch(getContacts(userId));
+        }
+    }, [dispatch, isLogged]);
 
-  const selectContact = (contact) => {
-    dispatch({ type: "SET_CONTACT_TO_CHAT", contact });
-  };
+    const selectContact = (contact) => {
+        dispatch({ type: "SET_CONTACT_TO_CHAT", contact });
+    };
 
-  // const retriveContactMessages = (contact) => {
-  //   const data = {
-  //     userId: userId,
-  //     contactId: contact,
-  //   };
-  //   dispatch(getMessages(data));
-  // };
+    // const retriveContactMessages = (contact) => {
+    //   const data = {
+    //     userId: userId,
+    //     contactId: contact,
+    //   };
+    //   dispatch(getMessages(data));
+    // };
 
-  return (
-    <div>
-      {contacts.length > 0 &&
-        contacts.map((contact, index) => (
-          <div
-            className="s-cont"
-            key={index}
-            onClick={() => {
-              selectContact(contact);
-              // retriveContactMessages(contact);
-            }}
-          >
-            <h6>{contact}</h6>
-          </div>
-        ))}
-    </div>
-  );
+    return (
+        <div>
+            {contacts.length > 0 &&
+                contacts.map((contact, index) => (
+                    <div
+                        className="s-cont"
+                        key={index}
+                        onClick={() => {
+                            selectContact(contact);
+                            // retriveContactMessages(contact);
+                        }}
+                    >
+                        <h6>{contact}</h6>
+                    </div>
+                ))}
+        </div>
+    );
 }
 
 export default HandlingContacts;
